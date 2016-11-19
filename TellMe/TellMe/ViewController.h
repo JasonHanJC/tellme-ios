1b14b70cc83543746c27e244d777f7d9549dc1be
Juncheng Han <namrie1990@gmail.com>
2016-11-19 10:30:42 -0800

    init the iOS app

---
+++ TellMe/TellMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TellMe
+//
+//  Created by Juncheng Han on 11/19/16.
+//  Copyright © 2016 Juncheng Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
